79fdb730d5d3dbf8238f76591796404e9c0eafed
Egor Starostin <egorst@gmail.com>
2015-04-04 23:01:13 +0300

    added 4 metrics; refresh screen every 15 seconds

--- oradash.js
+++ oradash.js
@@ -33,13 +33,24 @@ screen.key(['escape','q'], function (ch,key) { return process.exit(0); });
 
 box.focus;
 
+var timer = 0;
+
 function selectData(err,connection) {
     if (err) { console.error(err.message); return; }
-//    connection.execute("select sys_context('userenv','instance_name') from dual",displayData);
-    connection.execute("select value from gv$sysmetric where metric_name='Consistent Read Gets Per Sec' and round(intsize_csec/100)=15",displayData);
-    function displayData(err,result) {
-        if (err) { console.error(err.message); return; }
-        box.setContent('Consistent Read Gets Per Sec (last 15 sec) is {bold}'+result.rows[0][0]+'{/bold}');
-        screen.render();
-    }
+    setTimeout(function() {
+    //    connection.execute("select sys_context('userenv','instance_name') from dual",displayData);
+        connection.execute("select metric_name,value from gv$sysmetric where metric_name in ('Current OS Load','SQL Service Response Time','User Calls Per Sec','I/O Megabytes per Second') and round(intsize_csec/100)=60 order by metric_name,value",displayData);
+        timer = 15000;
+        function displayData(err,result) {
+            if (err) { console.error(err.message); return; }
+            var cntnt = "'"+result.rows[0][0]+"': {bold}"+result.rows[0][1].toFixed(4)+'{/bold}'+"\n";
+            cntnt    += "'"+result.rows[1][0]+"': {bold}"+result.rows[1][1].toFixed(4)+'{/bold}'+"\n";
+            cntnt    += "'"+result.rows[2][0]+"': {bold}"+result.rows[2][1].toFixed(4)+'{/bold}'+"\n";
+            cntnt    += "'"+result.rows[3][0]+"': {bold}"+result.rows[3][1].toFixed(4)+'{/bold}'+"\n";
+            box.setContent(cntnt);
+            screen.render();
+        }
+        selectData(err,connection);
+    }, timer
+    );
 }
